0a20d0e2189b595c7125582110617453d82d4821
Ahmed DEMIAI <demiaiahmed@gmail.com>
2022-03-04 20:17:07 +0100

    Test1

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/demiaiahmed/bookstoreAPI-gin-gorm/controllers"
+	"github.com/demiaiahmed/bookstoreAPI-gin-gorm/models"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	r := gin.Default()
+
+	r.GET("/", controllers.GetBooks)
+	models.ConnectDatabase()
+	r.Run()
 }
